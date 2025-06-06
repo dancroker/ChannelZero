aa40e4df7f4e690f03c8e500da7f07c82296f461
dancroker <160754622+dancroker@users.noreply.github.com>
2025-06-06 17:17:36 +0100

    Initial Godot commit

---
+++ channel-zero/main.gd
@@ -0,0 +1,10 @@
+extends Node2D
+func _ready():
+	print("Hello, world!")
+	var Map = []
+	for i in range(10):
+		var row = []
+		for j in range(10):
+			row.append("+")
+		Map.append(row)
+	print(Map)
