fc1402d76bdfb13c4084269a8958ec4131194ea0
dancroker <160754622+dancroker@users.noreply.github.com>
2025-06-09 22:52:12 +0100

    Added Map and Character Visuals
    Added: Basic versions of the map tiles
    Added: 3 Character Models
    Added: Basic front end visuals

---
+++ channel-zero/MapRenderer.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+func draw_map_visual(map):
+	var tilemap = get_parent() as TileMap
+	#if tilemap == null:
+	#	print("Error: Parent is not a TileMap!")
+	#	return
+	tilemap.clear()
+	for y in range(map.size()):
+		for x in range(map[0].size()):
+			var tile = map[y][x]
+			if tile == "#":
+				tilemap.set_cell(0, Vector2i(x, y), 0, Vector2i(0, 0))  # Wall Tile
+			elif tile == "x":
+				tilemap.set_cell(0, Vector2i(x, y), 0, Vector2i(2, 0))  # Enemy Team Spawn Tile
+			elif tile == "o":
+				tilemap.set_cell(0, Vector2i(x, y), 0, Vector2i(3, 0))  # Player Team Spawn Tile
+			else:
+				tilemap.set_cell(0, Vector2i(x, y), 0, Vector2i(1, 0))  # Floor Tile
--- channel-zero/main.gd
+++ channel-zero/main.gd
@@ -6,9 +6,52 @@ var Map = []
 var Visited = []
 
 func _ready():
-	var generated_map = GenerateMap(20, 20, 250)
+	var generated_map = GenerateMap(12, 12, 50)
+	const TilePush = 78.5 #Vector distance between middle of tiles
 	PrintMap(generated_map)
-	print("Done!")
+	var Team = LoadTeam(0,0,0)
+	var Member1 = Team[0]
+	var Member2 = Team[1]
+	var Member3 = Team[2]
+	var Member1Location = [0,0]
+	var Member2Location = [0,0]
+	var Member3Location = [0,0]
+	var Placed = false
+	for x in range(len(Map)):
+		for y in range(len(Map[0])):
+			if Map[x][y] == "o" and Placed == false:
+				Member1.global_position = Vector2(42+(TilePush*y), 36+(TilePush*x))
+				Member1.scale = Vector2(4.91, 4.91)
+				Member1Location = [y,x]
+				Placed = true
+	Placed = false
+	for x in range(len(Map)):
+		for y in range(len(Map[0])):
+			if Map[x][y] == "o" and Placed == false and [y,x] != Member1Location:
+				Member2.global_position = Vector2(42+(TilePush*y), 36+(TilePush*x))
+				Member2.scale = Vector2(4.91, 4.91)
+				Member2Location = [y,x]
+				Placed = true
+	Placed = false
+	for x in range(len(Map)):
+		for y in range(len(Map[0])):
+			if Map[x][y] == "o" and Placed == false and [y,x] != Member1Location and [y,x] != Member2Location:
+				Member3.global_position = Vector2(42+(TilePush*y), 36+(TilePush*x))
+				Member3.scale = Vector2(4.91, 4.91)
+				Placed = true
+	
+
+func LoadTeam(Member1File,Member2File,Member3File):
+	var Member1Scene = preload("res://Characters/RedKnight.tscn")
+	var Member1 = Member1Scene.instantiate()
+	add_child(Member1)
+	var Member2Scene = preload("res://Characters/BlueKnight.tscn")
+	var Member2 = Member2Scene.instantiate()
+	add_child(Member2)
+	var Member3Scene = preload("res://Characters/Hazmat.tscn")
+	var Member3 = Member3Scene.instantiate()
+	add_child(Member3)
+	return[Member1,Member2,Member3]
 
 func PathThroughMap(pMap):
 	Map = pMap
@@ -77,10 +120,20 @@ func MapValueCount(Map, MapValue):
 				PathCounter += 1
 	return PathCounter
 
-func SpawnLocations(Map, SpawnLocationCount):
-	while MapValueCount(Map, "o") != SpawnLocationCount:
+func EnemySpawnLocations(Map, SpawnLocationCount):
+	while MapValueCount(Map, "x") != SpawnLocationCount:
 		for x in range(Map.size() - 1, -1, -1):
 			for y in range(Map[0].size() - 1, -1, -1):
+				if Map[x][y] == ".":
+					if MapValueCount(Map, "x") == SpawnLocationCount:
+						pass
+					else:
+						Map[x][y] = "x"
+
+func PlayerSpawnLocations(Map, SpawnLocationCount):
+	while MapValueCount(Map, "o") != SpawnLocationCount:
+		for x in range(Map.size()):
+			for y in range(Map[0].size()):
 				if Map[x][y] == ".":
 					if MapValueCount(Map, "o") == SpawnLocationCount:
 						pass
@@ -91,5 +144,9 @@ func GenerateMap(RowCount, ColCount, PathCount):
 	var Map = []
 	while MapValueCount(Map, ".") != PathCount:
 		Map = FormRandomMap(RowCount, ColCount)
-	SpawnLocations(Map, 3)
+	PlayerSpawnLocations(Map,3)
+	EnemySpawnLocations(Map, 3)
+	$TileMap/MapRender.draw_map_visual(Map)
+	$TileMap.scale = Vector2(4.91, 4.91)
+	#$TileMap.position = Vector2(250, 0)
 	return Map
